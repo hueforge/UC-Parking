fdb1019675bfe2cd96dfb6d4e4c8e24036260fd6
hueforge <114825281+hueforge@users.noreply.github.com>
2022-12-08 12:51:20 -0500

    adding garage info

--- app/app.js
+++ app/app.js
@@ -5,31 +5,104 @@ createApp({
         return {
             requestStatus: 0,
             requestID: 0,
-            garageLocations: {
-                uptownWest: [2042,2043,2044,2045,2046,2047,2048,2049,2051,2060,2061],
-                uptownEast: [2050,2052]
-            },
-            parkingData: []
+            parkingData: [],
+            garageInfo: [
+                {
+                    name: 'uptown west',
+                    garages: [
+                        {
+                            id: 2042,
+                            name: 'University Ave Garage',
+                            available: undefined
+                        },
+                        {
+                            id: 2043,
+                            name: 'Corry Garage',
+                            available: undefined
+                        },
+                        {
+                            id: 2044,
+                            name: 'Varsity Village Garage',
+                            available: undefined
+                        },
+                        {
+                            id: 2045,
+                            name: 'CCM Garage',
+                            available: undefined
+                        },
+                        {
+                            id: 2046,
+                            name: 'Calhoun Garage',
+                            available: undefined
+                        },
+                        {
+                            id: 2047,
+                            name: 'Campus Green Garage',
+                            available: undefined
+                        },
+                        {
+                            id: 2048,
+                            name: 'Woodside Garage',
+                            available: undefined
+                        },
+                        {
+                            id: 2049,
+                            name: 'Clifton Court Garage',
+                            available: undefined
+                        },
+                        {
+                            id: 2051,
+                            name: 'Stratford Garage',
+                            available: undefined
+                        },
+                        {
+                            id: 2060,
+                            name: 'University Ave Garage Level 1',
+                            available: undefined
+                        },
+                        {
+                            id: 2061,
+                            name: 'University Park Apartments Garage',
+                            available: undefined
+                        }
+                    ]
+                },
+                {
+                    name: 'uptown east',
+                    garages: [
+                        {
+                            id: 2050,
+                            name: 'Eden Garage',
+                            available: '-'
+                        },
+                        {
+                            id: 2052,
+                            name: 'Kingsgate Garage',
+                            available: '-'
+                        }
+                    ]
+                }
+            ]
         }
     },
     beforeMount() {
-        this.getParkingData()
     },
     computed: {
         parkingApp() {
-            if (this.requestStatus === 1) {
-                this.parkingData.map(el => {
-                    el.Occupancy.map(occ => {if (occ.OccupancyType === "Transient") {el.Occupancy = occ}})
-                    if (el.FacilityID === '2061') {el.Description = 'University Park Apartments Garage'}
-                })
-                let parkingApp = this.parkingData
-                // let parkingApp = new Object()
-                // Object.keys(this.garageLocations).forEach(campus => {parkingApp[campus] = new Array()})
-                this.sortByAvailable(parkingApp)
-                return parkingApp.filter(el => {
-                    return parseInt(el.Occupancy.Available) > 0
-                })
-            }
+            // if (this.requestStatus === 1) {
+            //     this.parkingData.map(el => {
+            //         el.Occupancy.map(occ => {if (occ.OccupancyType === "Transient") {el.Occupancy = occ}})
+            //         if (el.FacilityID === '2061') {el.Description = 'University Park Apartments Garage'}
+            //     })
+            //     let parkingApp = this.parkingData
+            //     // let parkingApp = new Object()
+            //     // Object.keys(this.garageLocations).forEach(campus => {parkingApp[campus] = new Array()})
+            //     this.sortByAvailable(parkingApp)
+            //     return parkingApp.filter(el => {
+            //         return parseInt(el.Occupancy.Available) > 0
+            //     })
+            // }
+            return this.garageInfo
         }
     },
     methods: {
